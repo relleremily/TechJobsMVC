e5de234b59439ac8bcb7eb727512b76629054b73
Emily Reller <relleremily@hotmail.com>
2017-11-30 17:25:34 -0600

    Created Results method and table views

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -6,6 +7,7 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
@@ -15,6 +17,32 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+        //[Route("/Search/Results")]
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.searchType = searchType;
+                ViewBag.searchTerm = searchTerm;
+                ViewBag.jobs = jobs;
+
+                return View("Index");
+
+            }
+
+
+        }
 
     }
 }
